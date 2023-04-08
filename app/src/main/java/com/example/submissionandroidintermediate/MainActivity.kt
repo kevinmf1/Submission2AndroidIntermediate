a0103e4c98e9e22f1e34c8f071285092c268d595
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-04-08 22:32:17 +0700

    add binding and use the binding for make some logic in button like lihat password and daftar

--- app/src/main/java/com/example/submissionandroidintermediate/MainActivity.kt
+++ app/src/main/java/com/example/submissionandroidintermediate/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.submissionandroidintermediate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import com.example.submissionandroidintermediate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ifClicked()
+
+    }
+
+    private fun ifClicked() {
+        binding.btnLogin.setOnClickListener {
+//            val intent = Intent(this, HomeActivity::class.java)
+//            startActivity(intent)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.seePassword.setOnCheckedChangeListener { _, isChecked ->
+            binding.CVPassword.transformationMethod = if (isChecked) {
+                HideReturnsTransformationMethod.getInstance()
+            } else {
+                PasswordTransformationMethod.getInstance()
+            }
+            // Set selection to end of text
+            binding.CVPassword.text?.let { binding.CVPassword.setSelection(it.length) }
+        }
     }
 }
