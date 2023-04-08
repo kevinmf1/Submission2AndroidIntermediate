90ffad070433d84339b089239f0ff403a52a1e6b
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-04-08 22:37:11 +0700

    add validation for customview CVEmail.kt

--- app/src/main/java/com/example/submissionandroidintermediate/customview/CVEmail.kt
+++ app/src/main/java/com/example/submissionandroidintermediate/customview/CVEmail.kt
@@ -1,4 +1,53 @@
 package com.example.submissionandroidintermediate.customview
 
-class CVEmail {
+import android.content.Context
+import android.text.InputType
+import android.util.AttributeSet
+import android.util.Patterns
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.example.submissionandroidintermediate.R
+
+class CVEmail : AppCompatEditText, View.OnFocusChangeListener {
+
+    private var isEmailValid = false
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    private fun init() {
+        // Set background with border
+        background = ContextCompat.getDrawable(context, R.drawable.border)
+
+        // Set input type to email address
+        inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+
+        // Set onFocusChangeListener to validate email
+        onFocusChangeListener = this
+    }
+
+    override fun onFocusChange(v: View?, hasFocus: Boolean) {
+        if (!hasFocus) {
+            validateEmail()
+        }
+    }
+
+    private fun validateEmail() {
+        isEmailValid = Patterns.EMAIL_ADDRESS.matcher(text.toString().trim()).matches()
+        error = if (!isEmailValid) {
+            resources.getString(R.string.emailFormatWrong)
+        } else {
+            null
+        }
+    }
 }
