da391c9aea9dd9acb9075670a47f25fe112e6e55
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-04-08 14:44:16 +0700

    fix some code that was not work before (add new function and delete function that not used)

--- app/src/main/java/com/example/submissionandroidintermediate/customview/CVPassword.kt
+++ app/src/main/java/com/example/submissionandroidintermediate/customview/CVPassword.kt
@@ -1,6 +1,7 @@
 package com.example.submissionandroidintermediate.customview
 
 import android.content.Context
+import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.text.method.PasswordTransformationMethod
 import android.util.AttributeSet
@@ -9,34 +10,41 @@ import android.view.View
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.content.ContextCompat
 import com.example.submissionandroidintermediate.R
+
 class CVPassword : AppCompatEditText, View.OnTouchListener {
 
     private var isPasswordVisible = false
     private var showPasswordIcon: Drawable? = null
     private var hidePasswordIcon: Drawable? = null
     private var passwordVisibilityToggleEnabled: Boolean = true
+    private var isPasswordValid: Boolean = false
 
     init {
         init(null)
+        transformationMethod = PasswordTransformationMethod.getInstance()
     }
 
     constructor(context: Context) : super(context) {
         init(null)
+        transformationMethod = PasswordTransformationMethod.getInstance()
     }
 
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
         init(attrs)
+        transformationMethod = PasswordTransformationMethod.getInstance()
     }
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         init(attrs)
+        transformationMethod = PasswordTransformationMethod.getInstance()
     }
 
     private fun init(attrs: AttributeSet?) {
         // Load drawables for show/hide password icons
-        showPasswordIcon = ContextCompat.getDrawable(context, R.drawable.baseline_visibility_24)
-        hidePasswordIcon = ContextCompat.getDrawable(context, R.drawable.baseline_visibility_off_24)
+        showPasswordIcon = ContextCompat.getDrawable(context, R.drawable.baseline_visibility_off_24)
+        hidePasswordIcon = ContextCompat.getDrawable(context, R.drawable.baseline_visibility_24)
 
+        setCompoundDrawablesWithIntrinsicBounds(null, null, if (isPasswordVisible) hidePasswordIcon else showPasswordIcon, null)
         // Load attributes from XML
         attrs?.let {
             val typedArray = context.obtainStyledAttributes(it, R.styleable.CVPassword)
@@ -51,8 +59,8 @@ class CVPassword : AppCompatEditText, View.OnTouchListener {
     override fun onTouch(v: View?, event: MotionEvent): Boolean {
         if (passwordVisibilityToggleEnabled && event.action == MotionEvent.ACTION_UP) {
             // Check if touch event occurred within the bounds of the show/hide password icon
-            val isTouchWithinShowHideIconBounds = when {
-                layoutDirection == View.LAYOUT_DIRECTION_RTL -> event.x < showPasswordIcon!!.intrinsicWidth
+            val isTouchWithinShowHideIconBounds = when (layoutDirection) {
+                View.LAYOUT_DIRECTION_RTL -> event.x < showPasswordIcon!!.intrinsicWidth
                 else -> event.x > width - showPasswordIcon!!.intrinsicWidth
             }
 
@@ -69,16 +77,29 @@ class CVPassword : AppCompatEditText, View.OnTouchListener {
         if (isPasswordVisible) {
             // Show password
             transformationMethod = null
-            showPasswordIcon?.let { setButtonDrawables(it) }
+            showPasswordIcon?.let { setCompoundDrawablesWithIntrinsicBounds(null, null, hidePasswordIcon, null) }
         } else {
             // Hide password
             transformationMethod = PasswordTransformationMethod.getInstance()
-            hidePasswordIcon?.let { setButtonDrawables(it) }
+            hidePasswordIcon?.let { setCompoundDrawablesWithIntrinsicBounds(null, null, showPasswordIcon, null) }
+        }
+        invalidate()
+    }
+
+    override fun onFocusChanged(focused: Boolean, direction: Int, previouslyFocusedRect: Rect?) {
+        super.onFocusChanged(focused, direction, previouslyFocusedRect)
+        if (!focused) {
+            validatePassword()
         }
     }
 
-    private fun setButtonDrawables(endOfTheText: Drawable) {
-        setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, endOfTheText, null)
+    private fun validatePassword() {
+        isPasswordValid = (text?.length ?: 0) >= 6
+        error = if (!isPasswordValid) {
+            "Password must be at least 6 characters"
+        } else {
+            null
+        }
     }
-
 }
+
